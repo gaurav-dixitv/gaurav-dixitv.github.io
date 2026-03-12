84a7ba79e417c5f39b6de9bad30a51ccd0e3bb81
gaurav-dixitv <gaurav.dixitv@gmail.com>
2026-03-12 00:35:15 +0000

    Deploying to gh-pages from @ gaurav-dixitv/gaurav-dixitv.github.io@b3049b2cb5a73e23bd728e29cb61a59b7ffde35a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gaurav-dixitv/gaurav-dixitv-dev",
+    "data-repo": "gaurav-dixitv/gaurav-dixitv.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
